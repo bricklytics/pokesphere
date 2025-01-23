8da111d1ce136b04df81d68160216b297bfaa70c
nordael <jleandro.martins@gmail.com>
2025-01-23 18:44:59 -0300

    feat: updated network error handler

--- datalayer/src/main/java/com/bricklytics/pokesphere/datalayer/network/error/NetworkErrorHandler.kt
+++ datalayer/src/main/java/com/bricklytics/pokesphere/datalayer/network/error/NetworkErrorHandler.kt
@@ -4,15 +4,15 @@ import com.bricklytics.pokesphere.datalayer.base.error.ErrorDetailDTO
 import com.bricklytics.pokesphere.datalayer.base.error.ErrorHandler
 
 open class NetworkErrorHandler : ErrorHandler {
-    private var failure = ErrorDetailDTO()
+    private var failure: ErrorDetailDTO? = null
 
     override fun onFailure(errorDetails: ErrorDetailDTO) {
         this.failure = errorDetails
     }
 
-    fun getFailure(): ErrorDetailDTO {
+    fun getFailure(): ErrorDetailDTO? {
         return failure
     }
 
-    //criar um mecanismo de disparo de evento, e.g. LiveData, Flow, EventBus
+    fun anyFailure(): Boolean = failure != null
 }
