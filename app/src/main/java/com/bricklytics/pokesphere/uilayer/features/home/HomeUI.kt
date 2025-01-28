6fdfe69ae1d3343423b15ebed69e29b2c89b2236
nordael <jleandro.martins@gmail.com>
2025-01-28 19:34:08 -0300

    feat: updated basic ui

--- app/src/main/java/com/bricklytics/pokesphere/uilayer/features/home/HomeUI.kt
+++ app/src/main/java/com/bricklytics/pokesphere/uilayer/features/home/HomeUI.kt
@@ -2,19 +2,40 @@ package com.bricklytics.pokesphere.uilayer.features.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.bricklytics.pokesphere.uilayer.R
 import com.bricklytics.pokesphere.uilayer.base.navigation.AppRoutes
+import com.bricklytics.pokesphere.uilayer.components.features.navigationbar.PokeTopBar
+import com.bricklytics.pokesphere.uilayer.components.features.navigationbar.getNavigationBarHeight
+import com.bricklytics.pokesphere.uilayer.components.features.navigationbar.hasEdgeToEdgeSupport
 
+@Preview(showSystemUi = true, device = Devices.PIXEL_6)
+@Composable
+private fun HomePreview() {
+    MaterialTheme {
+        HomeUI(
+            navController = rememberNavController(),
+            viewModel = hiltViewModel<HomeViewModel>()
+        )
+    }
+}
 
 @Composable
 fun HomeUI(
@@ -23,16 +44,11 @@ fun HomeUI(
 ) {
     Scaffold(
         topBar = {
-            Row (
-                horizontalArrangement = Arrangement.Center,
-                modifier = Modifier.height(48.dp)
-            ){
-                Text(
-                    text = "Home",
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                )
-            }
+            PokeTopBar(
+                title = stringResource(R.string.home_title),
+                visibleIcon = false,
+                color = Color.Yellow
+            )
         },
         content = {
             Column(
@@ -46,16 +62,25 @@ fun HomeUI(
             }
         },
         bottomBar = {
-            Button(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .fillMaxWidth(),
-                onClick = {
-                    val route = AppRoutes.Pokemon.setArgument("bulbasaur")
-                    navController.navigate(route)
+            Column {
+                Button(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .fillMaxWidth(),
+                    onClick = {
+                        val route = AppRoutes.Pokemon.setArgument("bulbasaur")
+                        navController.navigate(route)
+                    }
+                ) {
+                    Text("Get Pokemon")
                 }
-            ) {
-                Text("Get Pokemon")
+                if (hasEdgeToEdgeSupport()) {
+                    Spacer(
+                        modifier = Modifier
+                            .height(getNavigationBarHeight())
+                            .fillMaxWidth()
+                    )
+                }
             }
         }
     )
--- app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/PokemonUI.kt
+++ app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/PokemonUI.kt
@@ -1,59 +1,64 @@
 package com.bricklytics.pokesphere.uilayer.features.pokemon
 
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.palette.graphics.Palette
+import com.bricklytics.pokesphere.domainlayer.features.pokemon.model.PokemonModel
+import com.bricklytics.pokesphere.uilayer.R
 import com.bricklytics.pokesphere.uilayer.base.navigation.AppRoutes
 import com.bricklytics.pokesphere.uilayer.components.features.bottomsheet.BottomSheet
 import com.bricklytics.pokesphere.uilayer.components.features.bottomsheet.ButtonOrientation
+import com.bricklytics.pokesphere.uilayer.components.features.card.PokeCard
+import com.bricklytics.pokesphere.uilayer.components.features.card.PokeCardSkeleton
+import com.bricklytics.pokesphere.uilayer.components.features.navigationbar.PokeTopBar
+import com.bricklytics.pokesphere.uilayer.components.fonts.psFontFamily
 import com.bricklytics.pokesphere.uilayer.features.pokemon.model.PokemonEvent
+import com.bricklytics.pokesphere.uilayer.features.pokemon.model.PokemonUIState
+import com.bumptech.glide.Glide
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 
 @Composable
 fun PokemonUI(navController: NavController, viewModel: PokemonViewModel) {
 
-    viewModel.load()
+    var colorTheme by remember { mutableStateOf(Color.White) }
 
     Scaffold(
         topBar = {
-            Row {
-                IconButton(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp, vertical = 24.dp)
-                        .weight(1f),
-                    onClick = {
-                        navController.navigate(AppRoutes.Home.route)
-                    }
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .background(
-                                color = Color.Green,
-                                shape = RoundedCornerShape(50)
-                            )
-                            .size(16.dp)
-                    )
-                }
-                Text(
-                    text = viewModel.uiState.pokemonName,
-                    modifier = Modifier
-                        .weight(3f)
-                        .fillMaxWidth()
-                )
-            }
+            PokeTopBar(
+                title = stringResource(R.string.pokemon_ui_title),
+                icon = null,
+                color = colorTheme,
+                onClickIcon = { navController.navigate(AppRoutes.Home.route) }
+            )
         },
         content = {
             Column(
@@ -61,31 +66,113 @@ fun PokemonUI(navController: NavController, viewModel: PokemonViewModel) {
                 modifier = Modifier.padding(it)
             ) {
                 viewModel.bottomSheetUiState.let { bottomSheet ->
-                    BottomSheet (
+                    BottomSheet(
                         visible = bottomSheet.enabled,
                         model = bottomSheet.model,
                         buttonOrientaion = ButtonOrientation.Vertical,
-                        primaryAction = "OK" to { viewModel.onEvent(PokemonEvent.OnDismissBottomSheet) },
+                        primaryAction = stringResource(R.string.all_action_ok) to {
+                            viewModel.onEvent(PokemonEvent.OnDismissBottomSheet)
+                        },
                         onDismiss = { viewModel.onEvent(PokemonEvent.OnDismissBottomSheet) }
                     )
                 }
 
-                Text(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(),
-                    text = "Home Screen"
+                PokemonGridList(
+                    viewModel.uiState,
+                    onEvent = { viewModel.onEvent(PokemonEvent.OnDrainedList) }
                 )
-
-                viewModel.uiState.pokemon.abilities.forEach { abilities ->
-                    Text(
-                        text = abilities.ability.name,
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .fillMaxWidth()
-                    )
-                }
             }
         },
     )
+}
+
+@Composable
+fun PokemonGridList(
+    uiState: PokemonUIState,
+    onEvent: () -> Unit,
+) {
+    val lazyGridState = rememberLazyGridState()
+
+    LaunchedEffect(lazyGridState.canScrollForward) {
+        snapshotFlow {
+            lazyGridState.layoutInfo.run {
+                visibleItemsInfo.lastOrNull()?.index == totalItemsCount - 1
+            }
+        }.collect { isScrolledToTheEnd ->
+            if (isScrolledToTheEnd && !lazyGridState.canScrollForward && lazyGridState.isScrollInProgress) {
+                onEvent()
+            }
+        }
+    }
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        state = lazyGridState,
+    ) {
+        if(uiState.isLoading) {
+            items(6) {
+                PokeCardSkeleton()
+            }
+        }
+
+        items(uiState.pokemonList.size) { index ->
+            PokeCard(
+                label = uiState.pokemonList[index].name,
+                imgUrl = uiState.pokemonList[index].officialArtworkModel.frontDefault,
+                onClick = {}
+            )
+//
+//            @OptIn(ExperimentalGlideComposeApi::class)
+//            Card(
+//                shape = RoundedCornerShape(8.dp),
+//                border = BorderStroke(2.dp, Color.White),
+//                modifier = Modifier
+//                    .background(Color.LightGray)
+//                    .padding(8.dp)
+//            ) {
+//                val list = uiState.pokemonList
+//                GlideImage(
+//                    model = list[index].officialArtworkModel.frontDefault,
+//                    contentDescription = null,
+//                    modifier = Modifier
+//                        .padding(4.dp)
+//                        .align(Alignment.CenterHorizontally),
+//                )
+//                Text(
+//                    text = list[index].name,
+//                    fontSize = 16.sp,
+//                    fontWeight = FontWeight.Normal,
+//                    fontFamily = psFontFamily,
+//                    modifier = Modifier
+//                        .padding(horizontal = 4.dp, vertical = 8.dp)
+//                        .align(Alignment.CenterHorizontally)
+//                )
+//            }
+        }
+    }
+}
+
+
+@Composable
+private fun getImage(
+    pokemon: PokemonModel,
+    context: Context
+): Pair<Color, Bitmap> {
+    val colorTheme = Color.Transparent
+    var bitmap by remember {
+        mutableStateOf(Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888))
+    }
+
+    LaunchedEffect(pokemon) {
+        bitmap = Glide.with(context)
+            .asBitmap()
+            .load(pokemon.officialArtworkModel.frontDefault)
+            .submit()
+            .get()
+
+        Palette.from(bitmap).generate().let {
+            Color(it.getDominantColor(Color.White.toArgb()))
+        }
+    }
+    return colorTheme to bitmap
 }
