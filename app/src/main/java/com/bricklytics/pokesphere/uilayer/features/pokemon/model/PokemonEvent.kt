35018279e6ba752ea6062ffa2307c084c3b77e42
nordael <jleandro.martins@gmail.com>
2025-01-28 19:33:45 -0300

    feat: updated data and event classes

--- app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/model/PokemonEvent.kt
+++ app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/model/PokemonEvent.kt
@@ -2,4 +2,5 @@ package com.bricklytics.pokesphere.uilayer.features.pokemon.model
 
 sealed class PokemonEvent {
     data object OnDismissBottomSheet : PokemonEvent()
+    data object OnDrainedList : PokemonEvent()
 }
--- app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/model/PokemonUIState.kt
+++ app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/model/PokemonUIState.kt
@@ -3,6 +3,10 @@ package com.bricklytics.pokesphere.uilayer.features.pokemon.model
 import com.bricklytics.pokesphere.domainlayer.features.pokemon.model.PokemonModel
 
 data class PokemonUIState(
+    val isLoading: Boolean = false,
     val pokemon: PokemonModel = PokemonModel(),
-    val pokemonName: String = ""
+    val pokemonList: MutableList<PokemonModel> = mutableListOf(),
+    val page: Int = 0,
+    val totalCount: Int  = 0,
+    val pokemonName: String = "",
 )
