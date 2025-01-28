a6e40a32707c6d91a27d2a1939921f4e1007551f
nordael <jleandro.martins@gmail.com>
2025-01-28 19:33:09 -0300

    feat: added use case and logic to fetch images

--- app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/PokemonViewModel.kt
+++ app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/PokemonViewModel.kt
@@ -1,17 +1,21 @@
 package com.bricklytics.pokesphere.uilayer.features.pokemon
 
+import androidx.annotation.VisibleForTesting
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bricklytics.pokesphere.uilayer.components.features.bottomsheet.model.BottomSheetModel
 import com.bricklytics.pokesphere.datalayer.network.AppDispatcher
 import com.bricklytics.pokesphere.datalayer.network.AppDispatchers
+import com.bricklytics.pokesphere.domainlayer.features.pokemon.model.PokemonModel
 import com.bricklytics.pokesphere.domainlayer.features.pokemon.usecase.PokemonGetDataFailureFactory
 import com.bricklytics.pokesphere.domainlayer.features.pokemon.usecase.PokemonGetDataUseCase
+import com.bricklytics.pokesphere.domainlayer.features.pokemon.usecase.PokemonGetListFailureFactory
+import com.bricklytics.pokesphere.domainlayer.features.pokemon.usecase.PokemonGetListUseCase
 import com.bricklytics.pokesphere.uilayer.R
 import com.bricklytics.pokesphere.uilayer.base.ResourcesProvider
+import com.bricklytics.pokesphere.uilayer.components.features.bottomsheet.model.BottomSheetModel
 import com.bricklytics.pokesphere.uilayer.features.pokemon.model.BottomSheetType
 import com.bricklytics.pokesphere.uilayer.features.pokemon.model.BottomSheetUIState
 import com.bricklytics.pokesphere.uilayer.features.pokemon.model.PokemonEvent
@@ -22,51 +26,76 @@ import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 @HiltViewModel(assistedFactory = PokemonViewModelFactory::class)
 class PokemonViewModel @AssistedInject constructor(
     @Assisted
     private val name: String,
-    private val getPokemonData: PokemonGetDataUseCase,
+    private val getPokemonDataUseCase: PokemonGetDataUseCase,
+    private val getPokemonListUseCase: PokemonGetListUseCase,
     @AppDispatcher(AppDispatchers.IO)
     private val dispatcher: CoroutineDispatcher,
     private val resources: ResourcesProvider
 ) : ViewModel() {
 
     var uiState by mutableStateOf(PokemonUIState())
-    private set
+        private set
 
     var bottomSheetUiState by mutableStateOf(BottomSheetUIState())
         private set
 
+    init { load() }
+
+    @VisibleForTesting
     fun load() {
+        getPokemonList()
+    }
+
+    @VisibleForTesting
+    fun getPokemonList() {
+        uiState = uiState.copy(isLoading = true)
         viewModelScope.launch(dispatcher) {
-            getPokemonData.fetch(
+            getPokemonListUseCase.fetch(
                 args = mapOf(
-                    "name" to name
+                    "page" to uiState.page
                 )
             ).onSuccess {
-                uiState = uiState.copy(
-                    pokemonName = name,
-                    pokemon = it
-                )
+                uiState = uiState.copy(totalCount = it.count)
+
+                val names = it.pokemonList
+                    .map { it.name }
+                    .toMutableList()
+
+                getPokemonByName(names)
             }.onFailure { error ->
-                when(error) {
-                    is PokemonGetDataFailureFactory.BaseFailure -> {
-                        bottomSheetUiState = bottomSheetUiState.copy(
-                            enabled = true,
-                            model = BottomSheetModel(
-                                title = resources.getString(R.string.pokemon_bottom_sheet_title),
-                                subTitle = resources.getString(R.string.pokemon_bottom_sheet_subtitle),
-                                footer = error.code.toString(),
-                                message = resources.getString(R.string.pokemon_bottom_sheet_description),
-                            ),
-                            errorType = BottomSheetType.Error
-                        )
-                    }
+                handlePokemonListError(error)
+            }
+        }
+    }
+
+    @VisibleForTesting
+    fun getPokemonByName(names: List<String>) {
+        val pokemons = mutableListOf<PokemonModel>()
+        names.forEach { name ->
+            runBlocking(dispatcher) {
+                getPokemonDataUseCase.fetch(
+                    args = mapOf(
+                        "name" to name
+                    )
+                ).onSuccess {
+                    pokemons.add(it)
+                }.onFailure { error ->
+                    handlePokemonGetDataError(error)
                 }
             }
         }
+        uiState = uiState.copy(
+            isLoading = false,
+            pokemonList = pokemons.apply {
+                addAll(0, uiState.pokemonList)
+            }
+        )
     }
 
     fun onEvent(event: PokemonEvent) {
@@ -74,6 +103,49 @@ class PokemonViewModel @AssistedInject constructor(
             is PokemonEvent.OnDismissBottomSheet -> {
                 bottomSheetUiState = bottomSheetUiState.copy(
                     enabled = false
+                )
+            }
+
+            is PokemonEvent.OnDrainedList -> {
+                if (uiState.page < uiState.totalCount) {
+                    uiState = uiState.copy(page = uiState.page + 1)
+                    getPokemonList()
+                }
+            }
+        }
+    }
+
+    @VisibleForTesting
+    fun <T> handlePokemonGetDataError(error: PokemonGetDataFailureFactory<T>) {
+        when (error) {
+            is PokemonGetDataFailureFactory.BaseFailure -> {
+                bottomSheetUiState = bottomSheetUiState.copy(
+                    enabled = true,
+                    model = BottomSheetModel(
+                        title = resources.getString(R.string.pokemon_bottom_sheet_title),
+                        subTitle = resources.getString(R.string.pokemon_bottom_sheet_subtitle),
+                        footer = error.code.toString(),
+                        message = resources.getString(R.string.pokemon_bottom_sheet_description),
+                    ),
+                    errorType = BottomSheetType.Error
+                )
+            }
+        }
+    }
+
+    @VisibleForTesting
+    fun <T> handlePokemonListError(error: PokemonGetListFailureFactory<T>) {
+        when (error) {
+            is PokemonGetListFailureFactory.BaseFailure -> {
+                bottomSheetUiState = bottomSheetUiState.copy(
+                    enabled = true,
+                    model = BottomSheetModel(
+                        title = resources.getString(R.string.pokemon_bottom_sheet_title),
+                        subTitle = resources.getString(R.string.pokemon_bottom_sheet_subtitle),
+                        footer = error.code.toString(),
+                        message = resources.getString(R.string.pokemon_bottom_sheet_description),
+                    ),
+                    errorType = BottomSheetType.Error
                 )
             }
         }
