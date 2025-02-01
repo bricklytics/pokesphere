f85460995fbd7546c850b65ff7556d3818917f8c
nordael <jleandro.martins@gmail.com>
2025-02-01 10:32:19 -0300

    feat: added preview; ajusted skeleton

--- app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/PokemonUI.kt
+++ app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/PokemonUI.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,7 +17,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import com.bricklytics.pokesphere.uilayer.R
 import com.bricklytics.pokesphere.uilayer.base.navigation.AppRoutes
 import com.bricklytics.pokesphere.uilayer.components.features.bottomsheet.BottomSheet
@@ -24,20 +28,58 @@ import com.bricklytics.pokesphere.uilayer.components.features.bottomsheet.Button
 import com.bricklytics.pokesphere.uilayer.components.features.card.PokeCard
 import com.bricklytics.pokesphere.uilayer.components.features.card.PokeCardSkeleton
 import com.bricklytics.pokesphere.uilayer.components.features.navigationbar.PokeTopBar
+import com.bricklytics.pokesphere.uilayer.features.pokemon.model.BottomSheetUIState
 import com.bricklytics.pokesphere.uilayer.features.pokemon.model.PokemonEvent
 import com.bricklytics.pokesphere.uilayer.features.pokemon.model.PokemonUIState
 
 @Composable
-fun PokemonUI(navController: NavController, viewModel: PokemonViewModel) {
+@Preview(showSystemUi = true, device = Devices.PIXEL_6)
+private fun PokemonUIPreview() {
+    MaterialTheme {
+        PokemonUIContent(
+            uiState = PokemonUIState(),
+            bottomSheetUiState = BottomSheetUIState(),
+            onEvent = {},
+            onClickBackIcon = {}
+        )
+    }
+}
 
-    var colorTheme by remember { mutableStateOf(Color.White) }
+
+@Composable
+fun PokemonUI(
+    navController: NavController,
+    viewModel: PokemonViewModel
+) {
+    PokemonUIContent(
+        uiState = viewModel.uiState,
+        bottomSheetUiState = viewModel.bottomSheetUiState,
+        onEvent = viewModel::onEvent,
+        onClickBackIcon = {
+            navController.navigate(
+                route = AppRoutes.Home.route,
+                navOptions = NavOptions.Builder().setRestoreState(false).build()
+            )
+        }
+    )
+
+}
+
+@Composable
+fun PokemonUIContent(
+    uiState: PokemonUIState,
+    bottomSheetUiState: BottomSheetUIState,
+    onEvent: (PokemonEvent) -> Unit,
+    onClickBackIcon: () -> Unit,
+) {
+    val colorTheme by remember { mutableStateOf(Color.White) }
 
     Scaffold(
         topBar = {
             PokeTopBar(
                 title = stringResource(R.string.pokemon_ui_title),
                 color = colorTheme,
-                onClickIcon = { navController.navigate(AppRoutes.Home.route) }
+                onClickIcon = onClickBackIcon
             )
         },
         content = {
@@ -45,21 +87,21 @@ fun PokemonUI(navController: NavController, viewModel: PokemonViewModel) {
                 verticalArrangement = Arrangement.Center,
                 modifier = Modifier.padding(it)
             ) {
-                viewModel.bottomSheetUiState.let { bottomSheet ->
+                bottomSheetUiState.let { bottomSheet ->
                     BottomSheet(
                         visible = bottomSheet.enabled,
                         model = bottomSheet.model,
                         buttonOrientaion = ButtonOrientation.Vertical,
                         primaryAction = stringResource(R.string.all_action_ok) to {
-                            viewModel.onEvent(PokemonEvent.OnDismissBottomSheet)
+                            onEvent(PokemonEvent.OnDismissBottomSheet)
                         },
-                        onDismiss = { viewModel.onEvent(PokemonEvent.OnDismissBottomSheet) }
+                        onDismiss = { onEvent(PokemonEvent.OnDismissBottomSheet) }
                     )
                 }
 
                 PokemonGridList(
-                    uiState = viewModel.uiState,
-                    onEvent = { viewModel.onEvent(PokemonEvent.OnDrainedList) }
+                    uiState = uiState,
+                    onEvent = onEvent
                 )
             }
         },
@@ -69,13 +111,13 @@ fun PokemonUI(navController: NavController, viewModel: PokemonViewModel) {
 @Composable
 fun PokemonGridList(
     uiState: PokemonUIState,
-    onEvent: () -> Unit,
+    onEvent: (PokemonEvent) -> Unit,
 ) {
     val lazyGridState = rememberLazyGridState()
 
     LaunchedEffect(lazyGridState.canScrollForward) {
-        if (!lazyGridState.canScrollForward && !uiState.isLoading) {
-            onEvent()
+        if (!lazyGridState.canScrollForward && lazyGridState.canScrollBackward && !uiState.isLoading) {
+            onEvent(PokemonEvent.OnDrainedList)
         }
     }
 
@@ -83,17 +125,22 @@ fun PokemonGridList(
         columns = GridCells.Fixed(2),
         state = lazyGridState,
     ) {
-        items(uiState.pokemonList.size) { index ->
-            if (uiState.isLoading) {
-               repeat(6){
-                    PokeCardSkeleton()
+
+        uiState.pokemonList.let {
+            if (it.isNotEmpty()) {
+                items(it.size) { index ->
+                    PokeCard(
+                        label = "#${it[index].id} ${it[index].name}",
+                        imgUrl = it[index].officialArtworkModel.frontDefault,
+                        isFavorite = it[index].isFavorite,
+                        onClick = { },
+                        onLongPress = {
+                            onEvent( PokemonEvent.OnLongPressCard(index) )
+                        }
+                    )
                 }
             } else {
-                PokeCard(
-                    label = uiState.pokemonList[index].name,
-                    imgUrl = uiState.pokemonList[index].officialArtworkModel.frontDefault,
-                    onClick = {}
-                )
+                items(6) { PokeCardSkeleton() }
             }
         }
     }
