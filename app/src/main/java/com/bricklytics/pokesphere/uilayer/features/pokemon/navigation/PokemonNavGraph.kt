996bb357c6a19facbe591eb8216dff2489cfaba8
nordael <jleandro.martins@gmail.com>
2025-01-31 21:25:24 -0300

    fix: adjusted contrast color on navbar

--- app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/navigation/PokemonNavGraph.kt
+++ app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/navigation/PokemonNavGraph.kt
@@ -1,6 +1,5 @@
 package com.bricklytics.pokesphere.uilayer.features.pokemon.navigation
 
-import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
