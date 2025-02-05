c6d56b71d30cc9806e76610373dd3678888f85f0
nordael <jleandro.martins@gmail.com>
2025-02-05 10:08:04 -0300

    refactor: inject dispatcher and provide base data source

--- datalayer/src/main/java/com/bricklytics/pokesphere/datalayer/di/DataSourceModule.kt
+++ datalayer/src/main/java/com/bricklytics/pokesphere/datalayer/di/DataSourceModule.kt
@@ -1,26 +1,50 @@
 package com.bricklytics.pokesphere.datalayer.di
 
 import com.bricklytics.pokesphere.datalayer.base.BaseApiDataSource
-import com.bricklytics.pokesphere.datalayer.features.ability.AbilityAPI
+import com.bricklytics.pokesphere.datalayer.features.ability.AbilityApi
 import com.bricklytics.pokesphere.datalayer.features.ability.AbilityApiDataSource
-import com.bricklytics.pokesphere.datalayer.features.pokemon.PokemonAPI
+import com.bricklytics.pokesphere.datalayer.features.pokemon.PokemonApi
 import com.bricklytics.pokesphere.datalayer.features.pokemon.PokemonApiDataSource
+import com.bricklytics.pokesphere.datalayer.network.AppDispatcher
+import com.bricklytics.pokesphere.datalayer.network.AppDispatchers
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal class DataSourceModule {
 
     @Provides
-    fun providePokemonDataSource(
-        pokemonAPI: PokemonAPI
-    ): BaseApiDataSource = PokemonApiDataSource(pokemonAPI)
+    fun providesBaseApiDataSource(
+        @AppDispatcher(AppDispatchers.IO)
+        dispatcher: CoroutineDispatcher
+    ): BaseApiDataSource = BaseApiDataSource(
+        ioDispatcher = dispatcher
+    )
 
     @Provides
+    @Singleton
+    fun providePokemonDataSource(
+        pokemonApi: PokemonApi,
+        @AppDispatcher(AppDispatchers.IO)
+        dispatcher: CoroutineDispatcher
+    ): BaseApiDataSource = PokemonApiDataSource(
+        pokemonApi = pokemonApi,
+        ioDispatcher = dispatcher
+    )
+
+    @Provides
+    @Singleton
     fun provideAbilityDataSource(
-        abilityAPI: AbilityAPI
-    ): BaseApiDataSource = AbilityApiDataSource(abilityAPI)
+        abilityAPI: AbilityApi,
+        @AppDispatcher(AppDispatchers.IO)
+        dispatcher: CoroutineDispatcher
+    ): BaseApiDataSource = AbilityApiDataSource(
+        abilityApi = abilityAPI,
+        ioDispatcher = dispatcher
+    )
 }
