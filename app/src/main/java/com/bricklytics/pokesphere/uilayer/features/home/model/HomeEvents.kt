e538a6e4b9093fe1f9288aaac0585e74b54ff734
nordael <jleandro.martins@gmail.com>
2025-02-07 15:10:47 -0300

    feat: added new events for navigation

--- app/src/main/java/com/bricklytics/pokesphere/uilayer/features/home/model/HomeEvents.kt
+++ app/src/main/java/com/bricklytics/pokesphere/uilayer/features/home/model/HomeEvents.kt
@@ -3,7 +3,10 @@ package com.bricklytics.pokesphere.uilayer.features.home.model
 import android.content.Context
 
 sealed class HomeEvents {
+    data object  OnBackPressed : HomeEvents()
     data object OnGetFavoritePokemon : HomeEvents()
+    data object OnHomePressed : HomeEvents()
+    data object OnSearchPokemon : HomeEvents()
 
     data class OnFetchPokemonImage(
         val context: Context
--- app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/model/PokemonEvent.kt
+++ app/src/main/java/com/bricklytics/pokesphere/uilayer/features/pokemon/model/PokemonEvent.kt
@@ -1,6 +1,7 @@
 package com.bricklytics.pokesphere.uilayer.features.pokemon.model
 
 sealed class PokemonEvent {
+    data object OnBackPressed : PokemonEvent()
     data object OnDismissBottomSheet : PokemonEvent()
     data object OnDrainedList : PokemonEvent()
 
