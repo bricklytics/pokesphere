a656042c937dc14935bcc945e7b01a300fe664ae
nordael <jleandro.martins@gmail.com>
2025-02-14 15:42:14 -0300

    feat: added navigation animated transition libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,6 +93,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation)
     debugImplementation(libs.androidx.ui.test.manifest)
 
     // Coroutine support
